a6a7676e1d06be44731ded9f8a63c85648710ab1
Karen <karenl.harper88@gmail.com>
2020-08-27 23:15:08 -0700

    Implemented status function

--- smallsh.c
+++ smallsh.c
@@ -2,24 +2,39 @@
 
 #include "smallsh.h"
 
-int main() {
+void reportStatus(int status) {
+    printf("exit value %d \n", status);
+} 
+
+void runShell() {
 
     char command[25];
 
     while(strcmp(command, "exit") != 0) {
         
-    printf(": ");
+        printf("command first value is %c \n", *command);      
+        
 
-    scanf("%s", command); 
+        printf(": ");
 
-    if (strcmp(command, "cd") == 0)
-        printf("change directory \n");
+        scanf("%s", command); 
 
-    else if (strcmp(command, "status") == 0)
-        printf("status query \n");
+        if (*command == '#') { // Comment support
+            continue;
+        }
+
+        else if (strcmp(command, "cd") == 0)
+            printf("change directory \n");
+
+        else if (strcmp(command, "status") == 0)
+            reportStatus(0);
 
     }
-         
+}
+
+int main() {
+
+    runShell();    
     
     return 0;
 }
---
+++ smallsh.h
@@ -0,0 +1,9 @@
+// Header files for the Small Shell Program
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <unistd.h>
+
+void runShell();
